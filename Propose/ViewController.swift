da56f6fa4fde50ebf7669c825e97544071f94d15
Mio0921 <m.nakamura2919@gmail.com>
2018-10-20 18:29:03 +0900

    Initial Commit

---
+++ Propose/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Propose
+//
+//  Created by 中村洋雄 on 2018/10/20.
+//  Copyright © 2018年 中村洋雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
