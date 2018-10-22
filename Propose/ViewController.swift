aae16b41ee15de677cd1739b0cfa9f30c7f9f079
Mio0921 <m.nakamura2919@gmail.com>
2018-10-22 21:51:43 +0900

    試しに

--- Propose/ViewController.swift
+++ Propose/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var haikeigazou: UIImageView!
+    
     override func viewDidLoad() {
+// 'func'とは　この場合だとviewDidLoadをどうするかという意味。画面を読み込んだときどんな処理をするか
+        
+     
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.Label.isHidden = true
+        //ラベルを隠す
+        
+        self.image1.isHidden = true
+        //画像を隠す
+        
+        self.haikeigazou.isHidden = false
+    
+        
     }
 
-
+    @IBAction func タップボタン(_ sender: Any) {
+        self.Label.isHidden = false
+        self.image1.isHidden = false
+        
+        
+    }
 }
+
+
 
